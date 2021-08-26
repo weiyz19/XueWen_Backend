a62d2a017e5caa6db0536b991c07af2238f3cc2d
weiyz19 <272068431@qq.com>
2021-08-26 10:05:38 +0800

    sucessfully deployed to server

--- src/main/java/com/example/test_mysql/TestMysqlApplication.java
+++ src/main/java/com/example/test_mysql/TestMysqlApplication.java
@@ -2,14 +2,21 @@ package com.example.test_mysql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @SpringBootApplication
-public class TestMysqlApplication {
+public class TestMysqlApplication extends SpringBootServletInitializer {
 
-  public static void main(String[] args) {
-    SpringApplication.run(TestMysqlApplication.class, args);
-  }
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(TestMysqlApplication.class);
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(TestMysqlApplication.class, args);
+	}
 
 }
---
+++ src/main/java/com/example/test_mysql/config/WebConfig.java
@@ -0,0 +1,49 @@
+/**
+ * weiyz19
+ * WebConfig.java
+ * 2021-08-24
+ */
+package com.example.test_mysql.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    /**
+     * 解决跨域问题
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("/*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
+    }
+
+//    private CorsConfiguration buildConfig() {
+//        CorsConfiguration corsConfiguration = new CorsConfiguration();
+//        corsConfiguration.addAllowedOrigin("*");
+//        corsConfiguration.addAllowedHeader("*");
+//        corsConfiguration.addAllowedMethod("*");
+//        corsConfiguration.addExposedHeader("Authorization");
+//        return corsConfiguration;
+//    }
+//
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", buildConfig());
+//        return new CorsFilter(source);
+//    }
+}
+
